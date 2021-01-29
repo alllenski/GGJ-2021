dfab1bc25e723f33530d28f33142db1655777ddd
DESKTOP-I7GUTAB\Sam <samueljireh.baquir@benilde.edu.ph>
2021-01-29 20:57:16 +0800

    Added Spawn Document

---
+++ Assets/Scripts/SpawnDocument.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnDocument : MonoBehaviour
+{
+    public GameObject document;
+    void OnMouseDown()
+    {
+        Instantiate(document, gameObject.transform.position, Quaternion.identity);
+    }
+
+}
