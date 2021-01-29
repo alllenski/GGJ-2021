1f061d4253e27b4767188caf3cf5e8bc83948cd0
alllenski <allenrada21@gmail.com>
2021-01-29 20:31:45 +0800

    Added unfinished returnee

--- Assets/Scripts/ReturneeBehavior.cs
+++ Assets/Scripts/ReturneeBehavior.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnItem : MonoBehaviour
+public class ReturneeBehavior : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameObject.GetComponent<LerpMovement>().moveTo(new Vector2(-8f, 2.5f), 3f);
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/SpawnItem.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnItem : MonoBehaviour
+{
+   
+    public GameObject prefab;
+
+    void spawnItem() 
+    {
+        GameObject item = Instantiate(prefab, gameObject.transform.position, Quaternion.identity);
+        item.GetComponent<LerpMovement>().moveTo(new Vector2(gameObject.transform.position.x,
+            gameObject.transform.position.y - 3.0f), 2.0f);
+    }
+
+}
